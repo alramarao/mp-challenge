eab366745e8dd8ae81b6cbe72db1106284663d80
Ramarao Akula <alramarao@gmail.com>
2024-02-22 20:39:41 +0100

    added new service for chocolate module API calls

---
+++ src/app/chocolates/services/chocolate.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Chocolate, ChocolatesTableList } from '../models';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChocolateService {
+
+  constructor(private httpClient: HttpClient) {}
+
+  getChocolates(pageNo: number): Observable<ChocolatesTableList> {
+    let params = new HttpParams().set('page', String(pageNo));
+    return this.httpClient.get<ChocolatesTableList>('/chocolates', { params });
+  }
+
+  updateChocolate(
+    id: string,
+    payload: Partial<Chocolate>
+  ): Observable<Chocolate> {
+    return this.httpClient.put<Chocolate>(`/chocolates/${id}`, payload);
+  }
+}
