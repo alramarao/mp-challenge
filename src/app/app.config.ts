15e5d6f998396ff6e24212f78ab0a79387f9aec4
Ramarao Akula <alramarao@gmail.com>
2024-02-22 20:41:12 +0100

    updated the config reference to use ngrx store and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
 import { interceptorProviders } from './interceptors';
+import { ChocolateEffects, ChocolateReducer } from './chocolates/state';
+import { provideState, provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors(interceptorProviders))
+    provideHttpClient(withInterceptors(interceptorProviders)),
+    provideEffects(ChocolateEffects),
+    provideStore(),
+    provideState({name: 'chocolate', reducer: ChocolateReducer}),
   ]
 };
